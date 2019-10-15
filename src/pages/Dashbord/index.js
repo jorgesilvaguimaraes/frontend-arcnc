5cf896ebabe45e6582960b1d4e6206c8cf6365a8
Jorge Guimarães <jorgeguimaraes@cinbesa.com.br>
2019-10-15 14:01:50 -0300

    add socketio

--- src/pages/Dashbord/index.js
+++ src/pages/Dashbord/index.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import socketio from 'socket.io-client';
 import api from "../../services/api";
 import "./styles.css";
+
 export default function Dashbord() {
   const [spots, setSpots] = useState([]);
+
+  useEffect(()=>{
+    const socket = socketio('http://localhost:3333');
+  },[])
 
   useEffect(() => {
     async function loadSpots() {
